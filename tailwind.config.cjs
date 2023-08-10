46c4e8875e9cee6a3814c757efd59e8f19f964c3
koshin <ekoshin2001@icloud.com>
2023-08-10 23:03:58 +0900

    styled for mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#111827",
+        secondary: "#737373",
+        accent: "#FFF056",
+      },
+    },
   },
-  plugins: [],
+
+  plugins: [require("@tailwindcss/typography")],
 };
