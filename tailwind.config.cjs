ecb605fc04619066a7dae62bb85208419bb5924b
koshin <ekoshin2001@icloud.com>
2023-08-19 08:43:59 +0900

    changed fonts setting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      'sans': ['MPLUS1p', 'ui-sans-serif', 'system-ui'],
+    },
     extend: {
       colors: {
         primary: "#111827",
