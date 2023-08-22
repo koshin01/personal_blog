a611cf1649a34495f8a35dd385dad71115a8499e
koshin <ekoshin2001@icloud.com>
2023-08-23 06:26:41 +0900

    changed hover action

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.dbkoshin.com",
-  integrations: [mdx(), sitemap(), tailwind(), preact()]
+  integrations: [mdx(), sitemap(), tailwind(), preact()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,13 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      'sans': ['MPLUS1p', 'ui-sans-serif', 'system-ui'],
+      sans: ["MPLUS1p", "ui-sans-serif", "system-ui"],
     },
     extend: {
       colors: {
         primary: "#111827",
         secondary: "#6b7280",
         accent: "#FFF056",
-        hoveredPrimary: "#585d67",
       },
     },
   },
